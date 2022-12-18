7d938653a59a9fef1ace734328c0632ab1ccaab3
Aliks175 <lesh-work@yandex.ru>
2022-12-18 15:40:58 +0300

    Добавьте файлы проекта.

---
+++ LessensCModel1_HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LessensCModel1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!!\n");
+            ReadLine();
+            Write("Hello");
+            Write(" World");
+            Write(" !!!");
+            ReadLine();
+        }
+    }
+}
