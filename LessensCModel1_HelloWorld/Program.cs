ca72d76cd4cd9fd2fe881746c812f469bdff595f
Aliks175 <lesh-work@yandex.ru>
2022-12-18 15:50:16 +0300

    ClearField

--- LessensCModel1_HelloWorld/Program.cs
+++ LessensCModel1_HelloWorld/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
+﻿using static System.Console;
 
 namespace LessensCModel1_HelloWorld
 {
@@ -16,7 +11,7 @@ namespace LessensCModel1_HelloWorld
             Write("Hello");
             Write(" World");
             Write(" !!!");
-            ReadLine();
+            ReadLine(); 
         }
     }
 }
