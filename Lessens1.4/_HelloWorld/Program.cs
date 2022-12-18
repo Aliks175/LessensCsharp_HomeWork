3b92e6834963dd4b70a3ad71bfaf2da40420009e
Aliks175 <lesh-work@yandex.ru>
2022-12-18 23:14:22 +0300

    Lessens1.4(Debug)

---
+++ Lessens1.4/_HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace _HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!!!\n");
+            ReadLine();
+        }
+    }
+}
---
+++ Lessens1.4/_WriteOrWriteLine/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace _WriteOrWriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Hello");
+            Write(" World");
+            Write(" !!!");
+            ReadLine();
+        }
+    }
+}
