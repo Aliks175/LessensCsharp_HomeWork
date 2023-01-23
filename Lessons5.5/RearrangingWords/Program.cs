3994258c6e71eb4be05b77e4c30c1f629711b114
Aliks175 <lesh-work@yandex.ru>
2023-01-23 19:49:35 +0300

    Lessons5.5(Debug)(Clear string)

--- Lessons5.5/RearrangingWords/Program.cs
+++ Lessons5.5/RearrangingWords/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace RearrangingWords
 {
@@ -6,89 +7,40 @@ namespace RearrangingWords
     {
         static void Main(string[] args)
         {
-            bool isplay = true;
-            while (isplay)
-            {
-                string enterText;
-                int valueBlankSpace = 0;
-                WriteLine("Enter your text");
-                enterText = ReadLine();
-                WriteLine("\n\t###-Start to work-###\n");
-                foreach (var item in enterText)
-                {
-                    if (item == ' ')
-                    {
-                        valueBlankSpace++;
-                    }
-                }
-                PrintWord(SearchWord(enterText, valueBlankSpace + 1));
-                ReadLine();
-                WriteLine("Exit to Program ?\nY or N");
-                string userSelection = ReadLine().ToUpper();
-                switch (userSelection)
-                {
-                    case "Y":
-                        isplay = false;
-                        break;
-                    case "N":
-                        break;
-                    default:
-                        WriteLine("Error\nPress Enter to Continue");
-                        ReadLine();
-                        break;
-                }
-                Clear();
-            }
+            string enterText;
+            string resultString;
+            WriteLine("Enter your text");
+            enterText = ReadLine();
+            WriteLine("\n\t###-Start to work-###\n");
+            resultString = ReversWords(enterText);
+            PrintWord(resultString);
+            ReadLine();
         }
 
-        static string[] SearchWord(string text, int valueWord)
+        static string ReversWords(string inputPhrase)
+        {
+            return Reverse(inputPhrase);
+        }
+
+        static string Reverse(string text)
         {
             string[] word;
-            int valueSaveWords = 0;
-            int firstIndexLetterWord = 0;
-            int endIndexLetterWord = 0;
-            word = new string[valueWord];
-            for (int i = 0; i < text.Length; i++)
+            string resultString = null;
+            char[] separators = new char[] { ' ', '.', ',', '?', '!' };
+            word = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = word.Length; i > 0; i--)
             {
-                if (text[i] == ' ')
+                if (word[i - 1] != null)
                 {
-                    endIndexLetterWord = i;
-                    word[valueSaveWords] = SaveWord(text, firstIndexLetterWord, endIndexLetterWord);
-                    valueSaveWords++;
-                    firstIndexLetterWord = endIndexLetterWord + 1;
+                    resultString += word[i - 1] + ' ';
                 }
             }
-            if (endIndexLetterWord < text.Length)
-            {
-                endIndexLetterWord = text.Length;
-                word[valueSaveWords] = SaveWord(text, firstIndexLetterWord, endIndexLetterWord);
-            }
-            return word;
+            return resultString;
         }
 
-        static string SaveWord(string text, int firstIndex, int endIndex)
+        static void PrintWord(string word)
         {
-            string word = null;
-            for (int i = firstIndex; i < endIndex; i++)
-            {
-                if (text[i] == ' ')
-                {
-                    continue;
-                }
-                else
-                {
-                    word += text[i];
-                }
-            }
-            return word;
-        }
-
-        static void PrintWord(string[] word)
-        {
-            for (int i = word.Length - 1; i > -1; i--)
-            {
-                Write($"{word[i]} ");
-            }
+            WriteLine(word + ' ');
         }
     }
 }
